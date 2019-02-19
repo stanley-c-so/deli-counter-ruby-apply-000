3bf282ad68954a11b990c1f87f4303dc7ac7584c
Stanley So <stanley.c.so@gmail.com>
2019-02-19 21:47:25 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -27,7 +27,7 @@ end
 def take_a_number(current_line, name)
   
   current_line.push(name)
-  puts "Welcome, " + name + ". You are number " + current_line.length + " in line."
+  puts "Welcome, " + name + ". You are number " + (current_line.length).to_s + " in line."
   
 end
 

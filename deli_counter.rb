248f13e559c9452666e5219fc21bd3850cb89a35
Stanley So <stanley.c.so@gmail.com>
2019-02-19 21:42:09 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,10 +1,35 @@
 # Write your code here.
 
-def line(current_line, name)
+def line(current_line)
+  
+  str = "The line is currently"
+  
+  if current_line.length == 0 then return str + " empty." end
+  
+  str += ": "
+  
+  current_line.each.with_index do |name, idx|
+    
+    if idx != current_line.length - 1
+      str += idx.to_s + ". " + name + " "
+    else
+      str += idx.to_s + ". " + name
+    end
+    
+  end
+  
+  return str
+  
 end
 
+
 def take_a_number(current_line, name)
+  
+  current_line.push(name)
+  return "Welcome, " + name + ". You are number " + current_line.length + " in line."
+  
 end
+
 
 def now_serving(current_line)
 end
